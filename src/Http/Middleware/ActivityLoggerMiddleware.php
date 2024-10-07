7060bb05f44181963a68509ecc05ff9476dd9f02
Rakibul Hasan <rakibulhasan@notionhive.com>
2024-10-07 11:33:18 +0600

    add error log

--- src/Http/Middleware/ActivityLoggerMiddleware.php
+++ src/Http/Middleware/ActivityLoggerMiddleware.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Validation\ValidationException;
 use Rakibul\Userlog\Models\ActivityLog;
 
 class ActivityLoggerMiddleware
@@ -13,13 +14,14 @@ class ActivityLoggerMiddleware
     public function handle($request, Closure $next)
     {
         $this->registerModelEventListeners();
+        $this->registerValidationErrorListener();
 
         return $next($request);
     }
 
     protected function registerModelEventListeners()
     {
-        $events = ['created', 'updated', 'deleted'];
+        $events = ['created', 'updated', 'deleted';
 
         foreach ($events as $event) {
             Event::listen("eloquent.{$event}: *", function ($eventName, array $data) use ($event) {
@@ -30,6 +32,31 @@ class ActivityLoggerMiddleware
                 $this->logModelEvent("{$event}.failed", $data[0]);
             });
         }
+    }
+
+    protected function registerValidationErrorListener()
+    {
+        app()->error(function (ValidationException $e) {
+            $this->logValidationError($e);
+        });
+    }
+
+    protected function logValidationError(ValidationException $e)
+    {
+        $user = Auth::user();
+        $logData = [
+            'user_id' => $user ? $user->id : null,
+            'user_name' => $user ? $user->name : null,
+            'title' => "{$e->response->originalUrl()} validation error",
+            'method' => $e->response->status(),
+            'status' => 'failed',
+            'path' => $e->response->originalUrl(),
+            'ip_address' => request()->ip(),
+            'user_agent' => request()->userAgent(),
+            'details' => json_encode($e->errors()),
+        ];
+
+        ActivityLog::create($logData);
     }
 
     protected function logModelEvent($event, Model $model)
